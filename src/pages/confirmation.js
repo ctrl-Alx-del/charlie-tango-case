705a5f0d07beaf6a83f16bd9f6097cc9068f0f27
Vijasan Vasantharajan <vijasan.v@hotmail.com>
2023-04-27 13:31:20 +0200

    some styling

--- src/pages/confirmation.js
+++ src/pages/confirmation.js
@@ -7,15 +7,35 @@ function Confirmation() {
   useEffect(() => {
     const timer = setTimeout(() => {
       router.push("/");
-    }, 2000);
+    }, 2500);
 
     return () => clearTimeout(timer);
   }, [router]);
 
+  const styles = {
+    container: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100vh",
+      backgroundColor: "#f8f8f8",
+    },
+    heading: {
+      fontSize: "2rem",
+      fontWeight: "bold",
+      marginBottom: "1rem",
+    },
+    message: {
+      fontSize: "1.2rem",
+      color: "#666",
+    },
+  };
+
   return (
-    <div>
-      <h1>Thanks!</h1>
-      <p>You will hear from us in 1-2 days.</p>
+    <div style={styles.container}>
+      <h1 style={styles.heading}>Thanks!</h1>
+      <p style={styles.message}>You will hear from us in 1-2 days.</p>
     </div>
   );
 }
