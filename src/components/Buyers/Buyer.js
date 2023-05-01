326f3345c33a65cd1f25403cd69698df398037b9
Lukas G <luka9938@stud.kea.dk>
2023-05-01 13:05:00 +0200

    buyer_fix

--- src/components/Buyers/Buyer.js
+++ src/components/Buyers/Buyer.js
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { DispatchContext } from "@/contexts/buyerContext";
+import { estateTypes } from "@/data/estateTypes";
 
 export default function Buyer(props) {
   return (
