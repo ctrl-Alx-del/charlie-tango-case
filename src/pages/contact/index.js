af3a95e43c72e4022fbbab801735a06306b47aea
steffen kroyer <stef5450@stud.kea.dk>
2023-05-01 11:10:52 +0200

    contact is mostly done

---
+++ src/pages/contact/index.js
@@ -0,0 +1,68 @@
+import Head from "next/head";
+import styles from "./Contact.module.css";
+
+export default function cont() {
+  return (
+    <>
+      <Head>
+        <title>Find buyer | EDC</title>
+      </Head>
+      <div className="wrapper">
+        <h1 className={styles.headline}>Hello MMD</h1>
+        <div className={styles.content}></div>
+      </div>
+      <section>
+        <from className="kontakt">
+          <div className="container">
+            <div className="container_box">
+              <div className="left">
+                <h2>Contact potential buyers</h2>
+
+                <label>
+                  <input
+                    placeholder="Name"
+                    type="text"
+                    className="field"
+                    name="Name"
+                    required
+                  />
+                </label>
+
+                <label>
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    className="field"
+                    name="email"
+                    required
+                  />
+                </label>
+
+                <label>
+                  <input
+                    placeholder="Phone"
+                    type="text"
+                    className="field"
+                    name="Phone"
+                    required
+                  />
+                </label>
+
+                <label>
+                  <input className="checkbox" type="checkbox" />
+                  <p>
+                    Yes please, EDC may contact me with offers and information
+                    related to the real estate market.
+                  </p>
+                </label>
+
+                <button className={styles.button}>Send</button>
+              </div>
+              <div className="right"></div>
+            </div>
+          </div>
+        </from>
+      </section>
+    </>
+  );
+}
