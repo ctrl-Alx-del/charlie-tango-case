cfc39f1c2814ee4ae7ae2067db8467ef8e8dd111
Alex <electromanati@gmail.com>
2023-05-01 11:31:52 +0200

    forms fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import styles from "./Home.module.css";
-import { useState } from "react";
 
 export default function Home() {
   return (
@@ -12,57 +11,42 @@ export default function Home() {
         <h1 className={styles.headline}>Seller's page</h1>
         <div className={styles.content}>
           <h2 className={styles.h2}>Find a buyer for your property</h2>
-          <p>
+          <p className={styles.p}>
             Here you can search after buyers for your property and sell it to
             the potential buyer.
           </p>
 
           <form action="/buyers" method="GET" className={styles.form}>
-            <label className="label">
-              <span className={styles.label}>Price</span>
-              <input name="price" required />
-            </label>
-            <label className="label">
-              <span className={styles.label}>Size of property</span>
-              <input name="propertySize"></input>
-            </label>
-            <label className="label">
+            <label>
               <span className={styles.label}>Zip Code</span>
-              <input name="zipCode" required />
+              <input className={styles.inputs} name="zipCode" required />
             </label>
-            <label className="estateType">
+            <label>
+              <span className={styles.label}>Price</span>
+              <input className={styles.inputs} name="price" required />
+            </label>
+            <label>
+              <span className={styles.label}>Size</span>
+              <input className={styles.inputs} name="size" required />
+            </label>
+            <label>
               <span className={styles.label}>Estate type</span>
-              <GetSelected />
+              <select className={styles.SelectType} name="estateType" required>
+                <option value="1">Villa</option>
+                <option value="2">Villalejlighed</option>
+                <option value="3">Rækkehus</option>
+                <option value="4">Ejerlejlighed</option>
+                <option value="5">Fritidshus</option>
+                <option value="6">Fritidsgrund</option>
+                <option value="7">Helårsgrund</option>
+                <option value="8">Andelsbolig</option>
+                <option value="9">Landejendom</option>
+              </select>
             </label>
-            <button className={styles.button}>Find potential buyers</button>
+            <button className={styles.button}>Submit</button>
           </form>
         </div>
       </div>
     </>
   );
 }
-
-function GetSelected() {
-  const [selectedValue, setSelectedValue] = useState("Villa");
-
-  function handleChange(event) {
-    setSelectedValue(event.target.value);
-  }
-
-  return (
-    <>
-      <select
-        name="houseTypes"
-        value={selectedValue}
-        onChange={handleChange}
-        className={styles.select}
-      >
-        <option value="Villa">Villa</option>
-        <option value="Apartment">Apartment</option>
-        <option value="Terraced house">Terraced house</option>
-      </select>
-    </>
-  );
-}
-
-function GetInput() {}
