47edcd80cd24a886d1f34d938ffb38b4065951dd
Lukas G <luka9938@stud.kea.dk>
2023-05-02 15:24:53 +0200

    small_fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import { Header } from "@/components/Header/Header";
-import { DispatchContext } from "@/contexts/buyerContext";
 import { StoreProvider } from "@/contexts/buyerContext";
 
 export default function App({ Component, pageProps }) {
