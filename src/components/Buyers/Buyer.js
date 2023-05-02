57a295298deb4e71ce5d68f014288172a61289ec
Lukas G <luka9938@stud.kea.dk>
2023-05-02 12:57:48 +0200

    numbers_fix

--- src/components/Buyers/Buyer.js
+++ src/components/Buyers/Buyer.js
@@ -1,3 +1,4 @@
+import { estateTypes } from "@/data/estateTypes";
 import React, { useState } from "react";
 
 export default function Buyer(props) {
@@ -7,14 +8,17 @@ export default function Buyer(props) {
     setSelected(!selected);
   };
 
+  let price = props.maxPrice;
+  let priceDots = price.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.");
+
   return (
     <div className="card">
       <h2>{props.name}</h2>
       <p>{props.description}</p>
-      <p>Price: {props.maxPrice}</p>
+      <p>Price: {priceDots} kr.</p>
       <p>Zip Code: {props.zipCode}</p>
-      <p>Estate Type: {props.estateType}</p>
-      <p>Size: {props.minSize}</p>
+      <p>Estate Type: {estateTypes[props.estateType - 1].name}</p>
+      <p>Size: {props.minSize} m²</p>
       <input type="checkbox" checked={selected} onChange={handleSelect} />
     </div>
   );
