2a47647b6354b2cf0dbbd06bea613596b0b4fe2e
Lukas G <luka9938@stud.kea.dk>
2023-05-03 10:24:27 +0200

    api_test

--- src/pages/api/add-order.js
+++ src/pages/api/add-order.js
@@ -1,16 +1,13 @@
 export default async function handler(req, res) {
-  const response = await fetch(
-    "https://vziisekssxtizavakpkw.supabase.co/rest/v1/webshop_order",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        apikey: process.env.SUPABASE_KEY,
-        Prefer: "return=representation",
-      },
-      body: JSON.stringify(req.body),
-    }
-  ).then((res) => res.json());
+  const response = await fetch("https://fbqdkjurwhlhzytwwqng.supabase.co", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      apikey: process.env.SUPABASE_KEY,
+      Prefer: "return=representation",
+    },
+    body: JSON.stringify(req.body),
+  }).then((res) => res.json());
   // res.redirect(307, "/");
   res.status(200).json({ response });
 }
